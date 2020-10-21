e8ecdda0ae926ed46a2462f85c57b3b4414e9dd7
FernandaCSX <73200160+FernandaCSX@users.noreply.github.com>
2020-10-20 21:39:11 -0300

    Update Engenheiro.cpp
    Mudar o modificador para private.
    Criar os construtores.
    Criar um override para função visualizacaoDados.
    Mover o atributo nome para classe Empregado.
    Utilizar métodos set e get.

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -2,10 +2,27 @@
 #include "Empregado.hpp"
 
 class Engenheiro : public Empregado {
-
+   
   public:
-    std::string nome;  
+    Engenheiro(): Empregado(), projetos(0) {}
+    
+    Engenheiro(std::string novoNome, double salario, int _projetos): Empregado(novoNome, salario), projetos(_projetos) {}
+    
+    void visualizacaoDados(double horas) {
+        std::cout << "Nome: " << nome << std::endl;
+        std::cout << "Salario Mes: " << pagamentoMes(horas) << std::endl;
+        std::cout << "Projetos: " << projetos << std::endl;
+        std::cout << std::endl;
+    }
+    void setProjetos(int _projetos){
+        projetos = _projetos;
+    }
+    int getProjetos(){
+        return projetos;
+    }
+    
+  private:
 	int projetos;
 	
+	
 };
-
