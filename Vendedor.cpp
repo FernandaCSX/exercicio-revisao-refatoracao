13684688e4f312093ea8d167a91ba97b94288c05
FernandaCSX <73200160+FernandaCSX@users.noreply.github.com>
2020-10-20 21:43:20 -0300

    Update Vendedor.cpp
    Mudar o modificador para private.
    Criar os construtores. 
    Sobrecarregar (dar varias definições para minha função) a função visualizacaoDados.
    (Minha função irá carregar varias definições -> por isso ela é virtual lá na classe base)
    Inserir quotaMensalVendas
    Utilizar métodos set e get.
    Criar uma variável para o numero doze, pois ele aparece como numero mágico. Traduzindo, não sabemos de onde o numero doze vem e o que ele referencia, pois isso será criada uma variável para ele. (Numero de meses.).

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -2,13 +2,34 @@
 #include "Empregado.hpp"
 
 class Vendedor : public Empregado {
-
   public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
+  
+    Vendedor(): Empregado(), quotaMensalVendas(0.0) {}
+    
+    Vendedor(std::string novoNome, double salario, double _quota): Empregado(novoNome, salario), quotaMensalVendas(_quota) {}
+    
+    double quotaTotalAnual() {
+        const int meses = 12;
+	    return quotaMensalVendas * meses;
 	}
+    
+    void visualizacaoDados(double horas){
+        std::cout << "Nome: " << nome << std::endl;
+        std::cout << "Salario Mes: " << pagamentoMes(horas) << std::endl;  
+        std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+        std::cout << std::endl;
+    }
+    
+    void setQuotaMensalVendas(double _quota){
+        quotaMensalVendas = _quota;
+    }
+    
+    double getQuotaMensalVendas(){
+        return quotaMensalVendas;
+    }
+    
+  private:
+	double quotaMensalVendas;
+	
 	
 };
-
